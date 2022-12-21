bf6448b1cfe33ca9de061c1673d18bbe5ef51cc6
mdsayedarif84 <mdsayedarif84@gmail.com>
2022-12-21 22:05:41 +0600

    middleware use for login check

--- fsMoon84/routes/web.php
+++ fsMoon84/routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\AboutController;
 use App\Http\Controllers\Admin\ServiceController;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Middleware\AdminAuthLoginCheck;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -25,29 +26,36 @@ Route::get('/',[FrontendController::class,'index'])->name('front.index');
 // Dashboard & auth login
 Auth::routes();
 Route::get('/home',[HomeController::class, 'index'])->name('home');
-Route::get('/category',[CategoryController::class, 'index'])->name('category');
-Route::post('/save/category',[CategoryController::class,'saveCategoryInfo'])->name('new.category');
-Route::get('/manage/category',[CategoryController::class,'manageCategoryInfo'])->name('manage.category');
-Route::get('/inactive/{id}',[CategoryController::class,'inactiveCategory'])->name('inactive.category');
-Route::get('/active/{id}',[CategoryController::class,'activeCategory'])->name('active.category');
-Route::get('/edit/category/{id}',[CategoryController::class,'editCategory'])->name('edit.category');
-Route::post('/update/category',[CategoryController::class,'updateCategory'])->name('update.category');
-Route::get('/delete/category/{id}',[CategoryController::class,'deleteCategory'])->name('delete.category');
 
-Route::get('/about',[AboutController::class, 'index'])->name('about');
-Route::post('/save/about',[AboutController::class,'saveAboutInfo'])->name('new.about');
-Route::get('/manage/about',[AboutController::class,'manageAbout'])->name('manage.about');
-Route::get('/inactive{id}',[AboutController::class,'inactiveAbout'])->name('inactive.about');
-Route::get('/active{id}',[AboutController::class,'activeAbout'])->name('active.about');
-Route::get('/edit/about/{id}',[AboutController::class,'editAbout'])->name('edit.about');
-Route::post('/update/about',[AboutController::class,'updateAbout'])->name('update.about');
-Route::post('/delete/about/{id}',[AboutController::class,'deleteAbout'])->name('delete.about');
 
-Route::get('/service',[ServiceController::class, 'index'])->name('add.service');
+// Route::group(['middleware'=>'login'],function(){
+Route::middleware(['login.check'])->group(function(){
+    Route::get('/category',[CategoryController::class, 'index'])->name('category');
+    Route::post('/save/category',[CategoryController::class,'saveCategoryInfo'])->name('new.category');
+    Route::get('/manage/category',[CategoryController::class,'manageCategoryInfo'])->name('manage.category');
+    Route::get('/inactive/{id}',[CategoryController::class,'inactiveCategory'])->name('inactive.category');
+    Route::get('/active/{id}',[CategoryController::class,'activeCategory'])->name('active.category');
+    Route::get('/edit/category/{id}',[CategoryController::class,'editCategory'])->name('edit.category');
+    Route::post('/update/category',[CategoryController::class,'updateCategory'])->name('update.category');
+    Route::get('/delete/category/{id}',[CategoryController::class,'deleteCategory'])->name('delete.category');
 
-Route::get('/user',[UserController::class, 'index'])->name('add.user');
-Route::post('/save/user',[UserController::class,'sotre'])->name('new.user');
-Route::get('/manage/user',[UserController::class,'manageUserInfo'])->name('manage.user');
+    Route::get('/about',[AboutController::class, 'index'])->name('about');
+    Route::post('/save/about',[AboutController::class,'saveAboutInfo'])->name('new.about');
+    Route::get('/manage/about',[AboutController::class,'manageAbout'])->name('manage.about');
+    Route::get('/inactive{id}',[AboutController::class,'inactiveAbout'])->name('inactive.about');
+    Route::get('/active{id}',[AboutController::class,'activeAbout'])->name('active.about');
+    Route::get('/edit/about/{id}',[AboutController::class,'editAbout'])->name('edit.about');
+    Route::post('/update/about',[AboutController::class,'updateAbout'])->name('update.about');
+    Route::post('/delete/about/{id}',[AboutController::class,'deleteAbout'])->name('delete.about');
+
+    Route::get('/service',[ServiceController::class, 'index'])->name('add.service');
+
+    Route::get('/user',[UserController::class, 'index'])->name('add.user');
+    Route::post('/save/user',[UserController::class,'sotre'])->name('new.user');
+    Route::get('/manage/user',[UserController::class,'manageUserInfo'])->name('manage.user');
+
+});
+
 
 
 
